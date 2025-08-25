6308bab2d7d2a0d974bd61b8cd8a9c0288556028
gonabenedict <gona.benedict24@students.dkut.ac.ke>
2025-08-25 12:30:05 +0300

    making authentication

---
+++ src/pages/auth/index.jsx
@@ -0,0 +1,9 @@
+export const Auth = () => {
+    return <div className="login-page"><p>
+        Sign in with google to continue
+        </p>
+        <button className="login-with-google-btn" onClick={SignInWithGoogle}>Sign in with Google
+            
+        </button>
+    </div>
+}
---
+++ src/pages/expense-tracker/index.jsx
@@ -0,0 +1,3 @@
+export const ExpenseTracker = () => {
+    return <div>Hello World expense</div>
+}
