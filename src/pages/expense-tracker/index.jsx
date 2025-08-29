7852defffe3ed481e0c51b894a2b8e011cbb7f42
gonabenedict <gona.benedict24@students.dkut.ac.ke>
2025-08-29 14:37:01 +0300

    added backend

--- src/pages/expense-tracker/index.jsx
+++ src/pages/expense-tracker/index.jsx
@@ -1,7 +1,18 @@
+import { useState } from 'react'
 import { useAddTransaction } from "../../hooks/useAddTransaction";
 
 export const ExpenseTracker = () => {
     const { addTransaction } = useAddTransaction();
+
+    const [descripton, setDescripton] = useState("");
+    const [transactonAmount, setTransactionAmount] = useState(0);
+    const [transactionType, setTransactionType] = usestate("expense");
+
+    const onSubmitn= async (e) => {
+        e.preventDefault()
+        addTransaction({descripton , transactionAmount, transactionType,})
+
+    }
 
     return (
         <>
@@ -24,12 +35,12 @@ export const ExpenseTracker = () => {
                     
 
                 </div>
-                <form className="add-transaction">
-                    <input type="text" placeholder="Descripton" required/>
-                    <input type="number" placeholder="Amount" required/>
-                    <input type="radio" id="expense" value="expense" />
+                <form className="add-transaction" onSubmit={onsubmit} >
+                    <input type="text" placeholder="Descripton" required onChange={(e) => setDescription(e.target.value)}/>
+                    <input type="number" placeholder="Amount" required onChange={(e) => setTransactionAmount(e.target.value)}/>
+                    <input type="radio" id="expense" value="expense" checked={transactionType === "expense"} onChange={(e) => setTransactionType(e.target.value)} />
                     <label htmlFor="expense"> Expense</label>
-                    <input type="radio" id="income" value="income"/>
+                        <input type="radio" id="income" value="income" checked={transactionType === "income"} onChange={(e) => setTransactionType(e.target.value)}/>
                     <label htmlFor="income"> Income</label>
 
                     <button type="submit"> Add Transaction</button>
